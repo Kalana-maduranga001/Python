99d177eccf44d329629bca12d90330e4178e1b7b
Kalana Maduranga <kalanamadu2000@gmail.com>
2025-11-21 11:16:21 +0530

    import differnt type

--- PACKAGE_TESTING/main.py
+++ PACKAGE_TESTING/main.py
@@ -1,8 +1,10 @@
-import my_Calcutator.addition as add
-import my_Calcutator.substraction as sub
+# import my_Calcutator.addition as add
+# import my_Calcutator.substraction as sub
 
-result1 = add.add( 5 , 4)
+from my_Calcutator.addition import add
+from my_Calcutator.substraction import substraction
+result1 = add( 5 , 4)
 print(result1)
 
-result2 = sub.substraction( 5 , 4)
+result2 = substraction( 5 , 4)
 print(result2)
