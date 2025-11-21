16139089d7a45f97cf3cf17eeda7e0c4d95009ce
Kalana Maduranga <kalanamadu2000@gmail.com>
2025-11-21 09:46:40 +0530

    math fuction

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import my_module
+from my_module import add
+from my_module import Factorial
+from my_module import SqureRoot
 
-result = my_module.add(3 , 5)
-
+result = add(3 , 5)
 print(f"The sum is : {result}")
+
+result2 = Factorial(5)
+print(f"The sum is : {result2}")
+
+
+result3 = SqureRoot(2)
+print(f"The sum is : {result3}")
--- my_module.py
+++ my_module.py
@@ -1,2 +1,10 @@
+import math
 def  add(a , b ):
     return a + b
+
+def Factorial(a):
+   return math.factorial(a)
+    
+def SqureRoot(b):
+  return math.sqrt(b)   
+    
